eb04eb92bb72c864a5f1a944e6ac019f5f1c2bbd
Lê Duy Nhật <55177671+leduynhat@users.noreply.github.com>
2020-06-04 09:39:56 +0700

    delete unnecessary files

--- AirSENSE_outdoor/src/ht_data.h
+++
@@ -1,8 +0,0 @@
-// void getHTUData()
-// {
-//   Temperature = htu.readTemperature();
-//   Humidity = htu.readHumidity();
-//   temperatureSum += Temperature;
-//   humiditySum += Humidity;
-//   HTUCount++;
-// }
